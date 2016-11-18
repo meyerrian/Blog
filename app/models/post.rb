bbeac9847b1561e1468b5cc8726e47c468f7d768
meyerrian <meyerrian@gmail.com>
2016-11-19 10:11:24 +1100

    google analytics code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: {medium: "300x300", large: "600x600", thumb: "150x150#", postshow: "80x80#"}
+  has_attached_file :image, styles: {medium: "300x300", large: "600x600", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
