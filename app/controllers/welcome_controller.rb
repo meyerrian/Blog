951ef503304376960c624fda2568f3860ff52a70
meyerrian <meyerrian@gmail.com>
2016-12-07 10:34:23 +1100

    styles for welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.all.order("created_at desc")
+    @posts = Post.all.order("created_at desc").limit(10)
     @projects = Project.all.order("created_at desc")
   end
 end
